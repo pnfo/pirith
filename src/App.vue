6dc630b6abcf64266ca0440f3187a41f60aa0d1a
pathnirvana <pathnirvana@gmail.com>
2023-09-09 19:18:02 +0700

    adding girimananda and dhammachakka suttas

--- src/App.vue
+++ src/App.vue
@@ -32,8 +32,8 @@
       <router-view></router-view>
       <v-sheet class="text-center ma-2" id="content-about">
         මේ‍ ව්‍යායාමය නාඋයනේ අරියධම්ම හිමියන්ගේ ශ්‍රී නාමයට කෙරෙන උපහාරයක්ම වේවා. පාලි පෙළ සහ සිංහල පරිවර්තනය උපුටා ගැනීම පිරුවානා පොත් වහන්සේගෙනි. 
-        උපකාර කල සියලුදෙනාට ද පින් අයත් වේවා. යම් දෝෂයක් ගැන අපව දැනුවත් කිරීමට pathnirvana<font-awesome-icon :icon="['far', 'at']" />gmail.com.<br>
-        මේ පිරිත් ඇතුලත් කර සැකසු අන්තර්ජාලය අනවශ්‍ය offline මෘදුකාංග <font-awesome-icon :icon="['far', 'laptop']" /> <a href="https://tipitaka.lk/library/459">පරිඝනක සඳහා</a> ලබාගන්න.<br>
+        උපකාර කල සියලුදෙනාට ද පින් අයත් වේවා. යම් දෝෂයක් ගැන අපව දැනුවත් කිරීමට pathnirvana<v-icon>mdi-at</v-icon>gmail.com.<br>
+        මේ පිරිත් ඇතුලත් කර සැකසු අන්තර්ජාලය අනවශ්‍ය offline මෘදුකාංග <v-icon>mdi-laptop</v-icon> <a href="https://tipitaka.lk/library/459">පරිඝනක සඳහා</a> ලබාගන්න.<br>
         ෴෴
       </v-sheet>
     </v-content>
--- src/main.js
+++ src/main.js
@@ -2,18 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/pro-solid-svg-icons'
-import { far } from '@fortawesome/pro-regular-svg-icons'
-import { fal } from '@fortawesome/pro-light-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faUserSecret } from '@fortawesome/pro-solid-svg-icons'
+// import { far } from '@fortawesome/pro-regular-svg-icons'
+// import { fal } from '@fortawesome/pro-light-svg-icons'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import vuetify from './plugins/vuetify';
-library.add(faUserSecret)
-library.add(fas, far, fal, fab)
+// library.add(faUserSecret)
+// library.add(fas, far, fal, fab)
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+//Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
